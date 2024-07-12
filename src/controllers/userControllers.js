f279edb2b9756a1771a705378563a2b3b151e6a4
PatrickW84 <allenpatrickwilliams@gmail.com>
2024-07-12 11:43:41 -0500

    login, register, and routes need testing

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,17 @@
+const { registerQuery } = require("../queries/userQuery");
+const { loginQuery } = require("../queries/userQuery");
+
+const register = async (req, res) => {
+  const token = await registerQuery(req.body);
+  res.send(token);
+};
+
+const login = async (req, res) => {
+  const token = await loginQuery(req.body);
+  res.send(token);
+};
+
+module.exports = {
+  register,
+  login,
+};
---
+++ src/queries/userQuery.js
@@ -0,0 +1,60 @@
+const { bcrypt, prisma, jwt } = require("../shared/shared");
+
+const registerQuery = async ({ firstName, lastName, email, password }) => {
+  const hashPassword = await bcrypt.hash(password, 10);
+  const registerUser = await prisma.user.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      password: hashPassword,
+    },
+  });
+  const token = jwt.sign(
+    {
+      id: registerUser.id,
+    },
+    process.env.WEB_TOKEN,
+    {
+      expiresIn: "1h",
+    }
+  );
+  return token;
+};
+
+// login function
+const loginQuery = async ({ email, password }) => {
+  const loginUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!loginUser) {
+    throw new Error("User not found");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, loginUser.password);
+
+  if (!isPasswordValid) {
+    throw new Error("Invalid password");
+  }
+
+  const token = jwt.sign(
+    {
+      id: loginUser.id,
+    },
+    process.env.WEB_TOKEN,
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  return token;
+};
+
+// export
+module.exports = {
+  registerQuery,
+  loginQuery,
+};
