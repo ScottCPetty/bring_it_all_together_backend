96c4f9792d6abefb7895ea28772325787c2783bc
ScottCPetty <scottishcheese13@gmail.com>
2024-07-16 10:59:55 -0400

    hopefully cors done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
-const cors = require("cors");
-app.use(cors());
 require("dotenv").config();
 const { app } = require("../src/shared/shared");
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`I am listening on port number ${PORT}`);
@@ -10,4 +16,3 @@ app.listen(PORT, () => {
 
 const userRoutes = require("../src/routes/userRoutes");
 app.use("/api/user", userRoutes);
-
